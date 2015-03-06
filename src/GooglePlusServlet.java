3f04bb8162262ae048953efa62246d9719db4473
achim-t <achim.t@gmail.com>
2015-03-06 17:29:58 +0100

    Initial Commit

---
+++ src/GooglePlusServlet.java
@@ -0,0 +1,44 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.builder.api.Google2Api;
+import org.scribe.oauth.OAuthService;
+
+/**
+ * Servlet implementation class GooglePlusServlet
+ */
+@WebServlet("/googleplus")
+public class GooglePlusServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final String CLIENT_ID = "331645637746-72r1bogrn14ud43p02cqhoq53vopi9ft.apps.googleusercontent.com"; 
+	   private static final String CLIENT_SECRET = "qq14xBzl0IjKmzKVI2HWUrT0";
+    
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServiceBuilder builder= new ServiceBuilder();
+		OAuthService service = builder.provider(Google2Api.class) 
+		         .apiKey(CLIENT_ID) 
+		         .apiSecret(CLIENT_SECRET) 
+		         .callback("http://localhost:8080/Youtube/oauth2callback") 
+		         .scope("https://www.googleapis.com/auth/youtube.readonly")  
+		    //     .debug(System.out) 
+		         .build(); //Now build the call
+		HttpSession session = request.getSession();
+		session.setAttribute("oauth2Service", service);
+		response.sendRedirect(service.getAuthorizationUrl(null));
+//		response.getWriter().println("Hello");
+	}
+
+}
---
+++ src/OAuth2CallbackServlet.java
@@ -0,0 +1,51 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
+import org.scribe.model.Verifier;
+import org.scribe.oauth.OAuthService;
+
+/**
+ * Servlet implementation class OAuth2CallbackServlet
+ */
+@WebServlet(asyncSupported = true, urlPatterns = { "/oauth2callback" })
+public class OAuth2CallbackServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String error = req.getParameter("error");
+		if ((error != null) && (error.trim().equals("access_denied"))){
+			HttpSession session = req.getSession();
+			session.invalidate();
+			resp.sendRedirect(req.getContextPath());
+			return;
+			
+		}
+		HttpSession session = req.getSession();
+		OAuthService service = (OAuthService) session.getAttribute("oauth2Service");
+		String code = req.getParameter("code");
+		Token token = service.getAccessToken(null, new Verifier(code));
+		session.setAttribute("token", token);
+		OAuthRequest oReq = new OAuthRequest(Verb.GET, "https://www.googleapis.com/youtube/v3/subscriptions?part=snippet&mine=true");
+		service.signRequest(token, oReq);
+		Response oResp = oReq.send();
+		String string = oResp.getBody();
+		resp.getWriter().println("Success?");
+		resp.getWriter().println(string);
+	}
+}
