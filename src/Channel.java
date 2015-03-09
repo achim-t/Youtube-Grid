081a09a0790df72761c3df963e4f684fd46fe709
achim-t <achim.t@gmail.com>
2015-03-09 10:03:42 +0100

    Refactor
    Factors out Channel class

---
+++ src/Channel.java
@@ -0,0 +1,32 @@
+import org.json.JSONObject;
+
+public class Channel {
+
+	private String thumbnailUrl;
+	private String title;
+	private String channelId;
+
+	public Channel(JSONObject channelJson) {
+		JSONObject channelSnippet = channelJson.getJSONObject("snippet");
+		title = channelSnippet.getString("title");
+		thumbnailUrl = channelSnippet.getJSONObject("thumbnails")
+				.getJSONObject("default").getString("url");
+		channelId = channelSnippet.getJSONObject("resourceId").getString(
+				"channelId");
+	}
+
+	public String getThumbnailUrl() {
+		return thumbnailUrl;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getChannelId() {
+		return channelId;
+	}
+	
+	
+
+}
--- src/Index.java
+++ src/Index.java
@@ -1,6 +1,4 @@
-
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
@@ -12,13 +10,11 @@ import javax.servlet.http.HttpSession;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.scribe.extractors.JsonTokenExtractor;
 import org.scribe.model.OAuthRequest;
 import org.scribe.model.Response;
 import org.scribe.model.Token;
 import org.scribe.model.Verb;
 import org.scribe.oauth.OAuthService;
-
 
 /**
  * Servlet implementation class Index
@@ -34,15 +30,16 @@ public class Index extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		
+
 		OAuthService service = (OAuthService) session
 				.getAttribute("oauth2Service");
 		OAuthRequest oReq = new OAuthRequest(Verb.GET,
 				"https://www.googleapis.com/youtube/v3/subscriptions?part=snippet&mine=true");
-		Token token = (Token)session.getAttribute("token");
-		if (service == null){
+		Token token = (Token) session.getAttribute("token");
+		if (service == null) {
 			System.out.println("token is null");
-			request.getRequestDispatcher("googleplus").forward(request, response);
+			request.getRequestDispatcher("googleplus").forward(request,
+					response);
 			return;
 		}
 		System.out.println("token is not null");
@@ -50,18 +47,18 @@ public class Index extends HttpServlet {
 		Response oResp = oReq.send();
 		PrintWriter writer = response.getWriter();
 		writer.println("<html><body>");
-		JSONObject jsonObject = new JSONObject(oResp.getBody()); 
+		JSONObject jsonObject = new JSONObject(oResp.getBody());
 		JSONArray jsonArray = jsonObject.getJSONArray("items");
-		for (int i=0; i<jsonArray.length(); i++){
+		for (int i = 0; i < jsonArray.length(); i++) {
+
 			writer.println("<div>");
-			JSONObject channel = jsonArray.getJSONObject(i);
-			JSONObject channelSnippet = channel.getJSONObject("snippet");
-			String title = channelSnippet.getString("title");
-			writer.println(title);
-			String imageUrl = channelSnippet.getJSONObject("thumbnails").getJSONObject("default").getString("url");
-			String channelId = channelSnippet.getJSONObject("resourceId").getString("channelId");
-			writer.println(channelId);
-			writer.println("<img src='"+imageUrl+"'/>");
+			JSONObject channelJson = jsonArray.getJSONObject(i);
+			Channel channel = new Channel(channelJson);
+
+			writer.println(channel.getTitle());
+
+			writer.println(channel.getChannelId());
+			writer.println("<img src='" + channel.getThumbnailUrl() + "'/>");
 			writer.println("</div>");
 		}
 		writer.println("</body></html>");
