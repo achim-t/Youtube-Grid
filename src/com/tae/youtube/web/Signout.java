23a6614cc8032cc6bdc3a004c7fb89a858afdbc0
achim-t <achim.t@gmail.com>
2015-03-18 09:04:12 +0100

    Redirect correctly when signing out

--- src/com/tae/youtube/web/Signout.java
+++ src/com/tae/youtube/web/Signout.java
@@ -19,7 +19,7 @@ public class Signout extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		req.getSession().invalidate();
-		req.getRequestDispatcher("./").forward(req, resp);;
+		req.getRequestDispatcher("./index").forward(req, resp);;
 	}
 
 	
