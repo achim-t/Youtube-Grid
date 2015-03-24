579d28e11da8e3f936dee29725ded61a33d1b8d5
achim-t <achim.t@gmail.com>
2015-03-24 13:00:57 +0100

    WS / deletes automated source comments

--- src/com/tae/youtube/web/InitListener.java
+++ src/com/tae/youtube/web/InitListener.java
@@ -11,31 +11,18 @@ import com.google.api.client.util.store.DataStore;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.tae.youtube.User;
 
-/**
- * Application Lifecycle Listener implementation class InitListener
- *
- */
 @WebListener
 public class InitListener implements ServletContextListener {
 
-	/**
-	 * Default constructor.
-	 */
 	public InitListener() {
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void contextDestroyed(ServletContextEvent event) {
-		// TODO Auto-generated method stub
-		// Map<String, User>users = (Map<String, User>)
-		// event.getServletContext().getAttribute("users");
 	}
 
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
-		// TODO Auto-generated method stub
-		// System.out.println(System.getProperty("user.home"));
 		FileDataStoreFactory fileDataStoreFactory;
 		try {
 			fileDataStoreFactory = new FileDataStoreFactory(new File(
@@ -46,7 +33,5 @@ public class InitListener implements ServletContextListener {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
-
 }
