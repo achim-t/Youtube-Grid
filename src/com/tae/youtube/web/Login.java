59e8161588b8d5351568b649859721ded0962ff5
achim-t <achim.t@gmail.com>
2015-03-24 11:18:47 +0100

    user is identified by his youtube-channelId

--- src/com/tae/youtube/web/Login.java
+++ src/com/tae/youtube/web/Login.java
@@ -9,7 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.util.store.DataStore;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.YouTube.Channels.List;
+import com.google.api.services.youtube.model.ChannelListResponse;
+import com.tae.youtube.Auth;
 import com.tae.youtube.User;
 
 @WebServlet("/login")
@@ -20,28 +25,42 @@ public class Login extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		HttpSession session = req.getSession();
-		User user = getUserFromSession(session);
-
-		System.out.println("user was created at: " + user.getCreatedAt());
-
-	}
-
-	private User getUserFromSession(HttpSession session) throws IOException {
-		String id = (String) session.getAttribute("youtube_id");
+		String youtubeId = (String) session.getAttribute("youtube_id");
 		DataStore<User> users = (DataStore<User>) getServletContext()
 				.getAttribute("users");
 		User user = null;
-		if (id != null && users.containsKey(id)) {
-			user = users.get(id);
-			System.out.println("returning user found");
-		} else {
-			user = new User();
-			String id2 = session.getId();
-			users.set(id2, user);
-			session.setAttribute("youtube_id", id2);
-			System.out.println("new user created");
+		
+		
+		if (youtubeId == null){
+			String userId = session.getId();
+			Credential credential = Auth.getCredential(userId);
+			if (credential == null) {
+				String url = Auth.getAuthorizationUrl();
+				resp.sendRedirect(url);
+				return;
+			}
+			YouTube youtube = new YouTube.Builder(Auth.HTTP_TRANSPORT,
+					Auth.JSON_FACTORY, credential).build();
+			ChannelListResponse list = youtube.channels().list("id").setMine(true).execute();
+			youtubeId = list.getItems().get(0).getId();
+			
 		}
-		return user;
+		
+		
+		if (youtubeId != null && users.containsKey(youtubeId)) {
+			user = users.get(youtubeId);
+			System.out.println("returning user found "+user.getId());
+		}
+		
+		if (user==null) {
+			user = new User();
+			user.setId(youtubeId);
+			users.set(youtubeId, user);
+			session.setAttribute("youtube_id", youtubeId);
+			System.out.println("new user created "+user.getId());
+		}
+		System.out.println("user was created at: " + user.getCreatedAt());
+
 	}
 
 }
