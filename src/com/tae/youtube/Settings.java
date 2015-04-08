63a0d3d0087c01b948c337b4ad71ad41a1a003c6
achim-t <achim.t@gmail.com>
2015-04-08 10:44:14 +0200

    fledged out settings a bit
    14: Persist User settings 
    
    Task-Url: https://github.com/achim-t/Youtube-Grid/issues/issue/14

--- src/com/tae/youtube/Settings.java
+++ src/com/tae/youtube/Settings.java
@@ -3,7 +3,19 @@ package com.tae.youtube;
 import java.io.Serializable;
 
 public class Settings implements Serializable {
-	public boolean getWatched(){
-		return true;
+	
+	private boolean watched=false;
+	private boolean filtered=false;
+	public boolean isWatched(){
+		return watched;
+	}
+	public boolean isFiltered() {
+		return filtered;
+	}
+	public void setFiltered(boolean filtered) {
+		this.filtered = filtered;
+	}
+	public void setWatched(boolean watched) {
+		this.watched = watched;
 	}
 }
