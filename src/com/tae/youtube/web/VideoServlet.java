6934d9d4845517f08b0755fae2cb8cb44c1ace12
achim-t <achim.t@gmail.com>
2015-04-08 09:26:54 +0200

    persists state of watched videos
    resolves #9

--- src/com/tae/youtube/web/VideoServlet.java
+++ src/com/tae/youtube/web/VideoServlet.java
@@ -1,11 +1,15 @@
 package com.tae.youtube.web;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.tae.youtube.User;
+import com.tae.youtube.YTVideo;
 
 /**
  * Servlet implementation class VideoServlet
@@ -29,10 +33,23 @@ public class VideoServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("user: "+request.getSession().getId());
+		String sessionId = request.getSession().getId();
+		User user = User.getBySessionId(sessionId);
+		if (user != null) {
+			String videoId = request.getParameter("id");
+			YTVideo video = user.getVideo(videoId);
+			String action = request.getParameter("action");
+			switch (action) {
+			case "mark":
+				video.setWatched(true);
+				break;
+			case "unmark":
+				video.setWatched(false);
+				break;
 
-		System.out.println("action: " + request.getParameter("action"));
-		System.out.println("id: " + request.getParameter("id"));
+			}
+
+		}
 	}
 
 }
