0f7e30c35346b5d65a5e2a78199399cb0d5b3d8a
achim-t <achim.t@gmail.com>
2015-04-16 12:07:05 +0200

    mark all as watched works as intended
    closes #12

--- WebContent/app.js
+++ WebContent/app.js
@@ -1,232 +1,225 @@
 var l = Ladda.create(document.querySelector('.ladda-button'));
-	function toggleWatched() {
+function toggleWatched() {
 
-		if (this.checked) {
-			$.ajax({
-				url : './user',
-				type : 'POST',
-				data : {
-					'setting' : 'showWatched'
-				}
-			});
-			$(".watched").show();
-		} else {
-			$.ajax({
-				url : './user',
-				type : 'POST',
-				data : {
-					'setting' : 'hideWatched'
-				}
-			});
-			$(".watched").hide();
-		}
-
+	if (this.checked) {
+		$.ajax({
+			url : './user',
+			type : 'POST',
+			data : {
+				'setting' : 'showWatched'
+			}
+		});
+		$(".watched").show();
+	} else {
+		$.ajax({
+			url : './user',
+			type : 'POST',
+			data : {
+				'setting' : 'hideWatched'
+			}
+		});
+		$(".watched").hide();
 	}
-	;
-	function toggleFiltered() {
 
-		if (this.checked) {
-			$.ajax({
-				url : './user',
-				type : 'POST',
-				data : {
-					'setting' : 'showFiltered'
-				}
-			});
-			$(".filtered").show();
-		} else {
-			$.ajax({
-				url : './user',
-				type : 'POST',
-				data : {
-					'setting' : 'hideFiltered'
-				}
-			});
-			$(".filtered").hide();
-		}
+};
+function toggleFiltered() {
 
+	if (this.checked) {
+		$.ajax({
+			url : './user',
+			type : 'POST',
+			data : {
+				'setting' : 'showFiltered'
+			}
+		});
+		$(".filtered").show();
+	} else {
+		$.ajax({
+			url : './user',
+			type : 'POST',
+			data : {
+				'setting' : 'hideFiltered'
+			}
+		});
+		$(".filtered").hide();
 	}
-	;
-	function markAs(video, state) {
-		video.addClass(state);
-		var $imgcontainer = $(".img-container", video);
+
+};
+function markAs(video, state) {
+	video.addClass(state);
+	var $imgcontainer = $(".img-container", video);
+	$imgcontainer.addClass("muted");
+	$imgcontainer.append($('<div class="watched-badge">' + state.toUpperCase()
+			+ '</div>'));
+}
+function markWatched(video) {
+	if (!$("#cbWatched").is(":checked")) {
+
+		video.hide("fast");
+	}
+	markAs(video, 'watched');
+	mark = $(".mark-watched", video);
+	mark.off();
+	mark.click(unwatched)
+
+	mark.attr("title", "Mark as Unwatched");
+	$.ajax({
+		url : './video',
+		type : 'POST',
+		data : {
+			'action' : 'mark',
+			'id' : video[0].id
+		}
+	});
+};
+function watched(e) {
+	e.preventDefault();
+	console.log("mark as watched");
+	var video = $(this).closest(".video");
+	markWatched(video);
+};
+function unwatched(e) {
+	e.preventDefault();
+	var $video = $(this).closest(".video");
+	$video.removeClass("watched")
+	var $imgcontainer = $(this).closest(".img-container");
+	$imgcontainer.removeClass("muted");
+	$('.watched-badge', $imgcontainer).remove();
+	$(this).off();
+	$(this).click(watched);
+	$(this).attr("title", "Mark as Watched");
+	$.ajax({
+		url : './video',
+		type : 'POST',
+		data : {
+			'action' : 'unmark',
+			'id' : $video[0].id
+		}
+	});
+}
+
+function reload() {
+	$('.container-fluid').empty()
+	l.start();
+	$.ajax({
+		url : './videoList'
+	}).done(function(responseJson) {
+		l.stop();
+		responseJson.reverse();
+		$.each(responseJson, createVideo);
+		refresh();
+	});
+}
+function refresh() {
+	console.log("trying to refresh Videos")
+	l.start();
+	$.ajax({
+		url : './refreshVideos'
+	}).done(function(responseJson) {
+		responseJson.reverse();
+		console.log("got response for refreshing videos")
+		$.each(responseJson, createVideo);
+		l.stop();
+	});
+}
+function createVideo2(index, data) {
+	var html = template(data);
+	var $video = $('<div>').html(html);
+	$video.appendTo($('.container-fluid'));
+};
+
+function createVideo(index, data) {
+	var $video = $('<div>', {
+		'class' : 'video',
+		'id' : data.id
+	});
+
+	var $imgcontainer = $('<div>', {
+		'class' : 'img-container'
+	}).appendTo($video);
+	var $a = $('<a>', {
+		'href' : "https://www.youtube.com/watch?v=" + data.id
+	}).click(function() {
+		markWatched($video);
+		window.open($a.attr('href'));
+		return false;
+	});
+	$a.appendTo($imgcontainer);
+	$('<img>', {
+		src : data.thumbnailUrl
+	}).appendTo($a);
+	$imgcontainer.append($('<span>', {
+		'class' : 'video-duration'
+	}).text(data.duration));
+	if (data.watched) {
+		$video.addClass('watched');
+		$imgcontainer.append($('<div>', {
+			'class' : 'watched-badge'
+		}).text("WATCHED"));
+		var $mark = $('<div>', {
+			'class' : 'mark-watched',
+			'title' : 'mark as unwatched'
+		}).click(unwatched);
+	} else {
+		var $mark = $('<div>', {
+			'class' : 'mark-watched',
+			'title' : 'mark as watched'
+		}).click(watched);
+	}
+
+	$mark.appendTo($imgcontainer);
+	$('<span>', {
+		'class' : "glyphicon glyphicon-ok",
+		'aria-hidden' : 'true'
+	}).appendTo($mark);
+	if (data.filtered) {
+		$video.addClass('filtered');
+		$imgcontainer.append($('<div>', {
+			'class' : 'watched-badge'
+		}).text("FILTERED"));
+	} else {
+		var $filter = $('<div>', {
+			'class' : 'filter',
+			'data-toggle' : 'modal',
+			'data-target' : '#filterModal',
+			'data-title' : data.title,
+			'data-channel' : data.channelId,
+			'data-channelname' : data.channelName,
+			'data-id' : data.id,
+			'title' : 'Filter Videos like this'
+		}).appendTo($imgcontainer);
+		$('<span>', {
+			'class' : "glyphicon glyphicon-filter",
+			'aria-hidden' : 'true'
+		}).appendTo($filter);
+	}
+	$video.append($('<div>', {
+		'class' : 'title'
+	}).text(data.title));
+	$video.append($('<div>', {
+		'class' : 'byline'
+	}).text("by ").append($('<span>', {
+		'class' : 'channel'
+	}).text(data.channelName)));
+	$('.container-fluid').prepend($video);
+	if (data.watched) {
 		$imgcontainer.addClass("muted");
-		$imgcontainer.append($('<div class="watched-badge">'
-				+ state.toUpperCase() + '</div>'));
-	}
-	function markWatched(video) {
 		if (!$("#cbWatched").is(":checked")) {
 
-			video.hide("fast");
-		}
-		markAs(video, 'watched');
-		mark = $(".mark-watched", video);
-		mark.off();
-		mark.click(unwatched)
-
-		mark.attr("title", "Mark as Unwatched");
-		$.ajax({
-			url : './video',
-			type : 'POST',
-			data : {
-				'action' : 'mark',
-				'id' : video[0].id
-			}
-		});
-	}
-	;
-	function watched(e) {
-		e.preventDefault();
-		console.log("mark as watched");
-		var video = $(this).closest(".video");
-		markWatched(video);
-	}
-	;
-	function unwatched(e) {
-		e.preventDefault();
-		var $video = $(this).closest(".video");
-		$video.removeClass("watched")
-		var $imgcontainer = $(this).closest(".img-container");
-		$imgcontainer.removeClass("muted");
-		$('.watched-badge', $imgcontainer).remove();
-		$(this).off();
-		$(this).click(watched);
-		$(this).attr("title", "Mark as Watched");
-		$.ajax({
-			url : './video',
-			type : 'POST',
-			data : {
-				'action' : 'unmark',
-				'id' : $video[0].id
-			}
-		});
-	}
-
-	
-	function reload() {
-		$('.container-fluid').empty()
-		l.start();
-		$.ajax({
-			url : './videoList'
-		}).done(function(responseJson) {
-			l.stop();
-			responseJson.reverse();
-			$.each(responseJson, createVideo);
-			refresh();
-		});
-	}
-	function refresh() {
-		console.log("trying to refresh Videos")
-		l.start();
-		$.ajax({
-			url : './refreshVideos'
-		}).done(function(responseJson) {
-			responseJson.reverse();
-			console.log("got response for refreshing videos")
-			$.each(responseJson, createVideo);
-			l.stop();
-		});
-	}
-	function createVideo2(index, data) {
-		var html = template(data);
-		var $video = $('<div>').html(html);
-		$video.appendTo($('.container-fluid'));
-	}
-	;
-
-	function createVideo(index, data) {
-		var $video = $('<div>', {
-			'class' : 'video',
-			'id' : data.id
-		});
-
-		var $imgcontainer = $('<div>', {
-			'class' : 'img-container'
-		}).appendTo($video);
-		var $a = $('<a>', {
-			'href' : "https://www.youtube.com/watch?v=" + data.id
-		}).click(function() {
-			markWatched($video);
-			window.open($a.attr('href'));
-			return false;
-		});
-		$a.appendTo($imgcontainer);
-		$('<img>', {
-			src : data.thumbnailUrl
-		}).appendTo($a);
-		$imgcontainer.append($('<span>', {
-			'class' : 'video-duration'
-		}).text(data.duration));
-		if (data.watched) {
-			$video.addClass('watched');
-			$imgcontainer.append($('<div>', {
-				'class' : 'watched-badge'
-			}).text("WATCHED"));
-			var $mark = $('<div>', {
-				'class' : 'mark-watched',
-				'title' : 'mark as unwatched'
-			}).click(unwatched);
-		} else {
-			var $mark = $('<div>', {
-				'class' : 'mark-watched',
-				'title' : 'mark as watched'
-			}).click(watched);
-		}
-
-		$mark.appendTo($imgcontainer);
-		$('<span>', {
-			'class' : "glyphicon glyphicon-ok",
-			'aria-hidden' : 'true'
-		}).appendTo($mark);
-		if (data.filtered) {
-			$video.addClass('filtered');
-			$imgcontainer.append($('<div>', {
-				'class' : 'watched-badge'
-			}).text("FILTERED"));
-		} else {
-			var $filter = $('<div>', {
-				'class' : 'filter',
-				'data-toggle' : 'modal',
-				'data-target' : '#filterModal',
-				'data-title' : data.title,
-				'data-channel' : data.channelId,
-				'data-channelname' : data.channelName,
-				'data-id' : data.id,
-				'title' : 'Filter Videos like this'
-			}).appendTo($imgcontainer);
-			$('<span>', {
-				'class' : "glyphicon glyphicon-filter",
-				'aria-hidden' : 'true'
-			}).appendTo($filter);
-		}
-		$video.append($('<div>', {
-			'class' : 'title'
-		}).text(data.title));
-		$video.append($('<div>', {
-			'class' : 'byline'
-		}).text("by ").append($('<span>', {
-			'class' : 'channel'
-		}).text(data.channelName)));
-		$('.container-fluid').prepend($video);
-		if (data.watched) {
-			$imgcontainer.addClass("muted");
-			if (!$("#cbWatched").is(":checked")) {
-
-				$video.hide();
-			}
-		}
-		if (data.filtered) {
-			$imgcontainer.addClass("muted");
-			if (!$("#cbFiltered").is(":checked")) {
-
-				$video.hide();
-			}
+			$video.hide();
 		}
 	}
+	if (data.filtered) {
+		$imgcontainer.addClass("muted");
+		if (!$("#cbFiltered").is(":checked")) {
+
+			$video.hide();
+		}
+	}
+}
 
 $(function() {
 	var template = Handlebars.compile($('#template').html());
-
 
 	$('#filterModal').on(
 			'show.bs.modal',
@@ -272,15 +265,12 @@ $(function() {
 	$("#cbWatched").on("change", toggleWatched);
 	$("#cbFiltered").on("change", toggleFiltered);
 	$('#btnMarkAll').on("click", function() {
-		$('.video').addClass("muted");
-		if (!$("#cbWatched").is(":checked")) {
-
-			$('.video').hide();
-		}
+		$('.video').filter(":visible").each(function() {
+			markWatched($(this))
+		})
 	});
 
 	$('#btnRefresh').on("click", refresh);
 
-	
 	reload()
 });
