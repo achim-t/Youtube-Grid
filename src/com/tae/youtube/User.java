5bf155f2a3bb1c2824ba7194d342b515afb4caca
achim-t <achim.t@gmail.com>
2015-04-23 09:01:25 +0200

    resolves #2
    some cleanup

--- src/com/tae/youtube/User.java
+++ src/com/tae/youtube/User.java
@@ -28,14 +28,12 @@ import com.google.api.services.youtube.model.SubscriptionListResponse;
 import com.google.api.services.youtube.model.Video;
 import com.google.api.services.youtube.model.VideoListResponse;
 
-
-
 @Entity
 public class User {
-	@OneToMany(cascade=CascadeType.ALL)
-	private Map<String, YTVideo> videos= new HashMap<>();
-	@OneToMany(cascade=CascadeType.ALL)
-	private Map<String, Channel> subscriptions=new HashMap<>();;
+	@OneToMany(cascade = CascadeType.ALL)
+	private Map<String, YTVideo> videos = new HashMap<>();
+	@OneToMany(cascade = CascadeType.ALL)
+	private Map<String, Channel> subscriptions = new HashMap<>();;
 	@Id
 	private String youtubeId;
 	@Transient
@@ -43,11 +41,8 @@ public class User {
 	private String name;
 	@OneToOne(cascade = CascadeType.ALL)
 	private Settings settings = new Settings();
-	private Map<String, Collection<String>> filters= new HashMap<>();
-	
-	
-
-	
+	private Map<String, Collection<String>> filters = new HashMap<>();
+	private static final int BATCH_SIZE = 20;
 
 	public void setYoutube(YouTube youtube) {
 		this.youtube = youtube;
@@ -61,7 +56,7 @@ public class User {
 		return name;
 	}
 
-	public YouTube getYoutube(String sessionId) throws IOException {
+	private YouTube getYoutube(String sessionId) throws IOException {
 		if (youtube == null) {
 			Credential credential = Auth.getCredential(sessionId);
 
@@ -81,30 +76,25 @@ public class User {
 
 	private List<Channel> getActiveSubscriptions() throws IOException {
 		List<Channel> currentSubscriptions = getSubscriptionsFromYouTube();
-		
+
 		for (Channel subscription : subscriptions.values()) {
 			if (!currentSubscriptions.contains(subscription))
 				subscription.setActive(false);
 		}
-		
+
 		for (Channel subscription : currentSubscriptions) {
-			
 			String channelId = subscription.getChannelId();
 			if (!subscriptions.containsKey(channelId)) {
 				subscriptions.put(channelId, subscription);
-			}
-			else 
+			} else {
 				subscriptions.get(channelId).setActive(true);
-		
+			}
 		}
 		return currentSubscriptions;
 	}
 
-	
-
 	public List<YTVideo> getSavedVideos() {
-		List<YTVideo> videoList = new ArrayList<>();
-		videoList.addAll(videos.values());
+		List<YTVideo> videoList = new ArrayList<>(videos.values());
 		doFilterVideos(videoList);
 		return videoList;
 	}
@@ -120,7 +110,6 @@ public class User {
 				result.add(video);
 				videos.put(video.getVideoId(), video);
 			}
-
 		}
 		doFilterVideos(result);
 		EntityManager em = Application.getFactory().createEntityManager();
@@ -150,42 +139,51 @@ public class User {
 	private List<YTVideo> getVideosFromYoutube(Collection<Channel> channels)
 			throws IOException {
 		List<YTVideo> videos = new ArrayList<>();
-		List<String> ids = getIds(channels);
-		
-		for (int i = 0; i * 50 < ids.size(); i++) {
-			int start = 50 * i;
-			int end = start + 50 < ids.size() ? start + 50 : ids.size();
-			List<String> subList = ids.subList(start, end);
-			String idsString = String.join(",", subList);
-			VideoListResponse videoListResponse = youtube.videos()
-					.list("snippet,contentDetails").setId(idsString).execute();
+		List<String> ids = getVideoIds(channels);
 
-			for (Video v : videoListResponse.getItems()) {
-				YTVideo video = new YTVideo(v);
-				Channel channel = getChannel(video.getChannelId());
-				video.setChannelName(channel.getTitle());
-				videos.add(video);
-				if (video.getPublishedAt().getValue() > channel
-						.getLastRefreshTime().getValue()) {
-					channel.setLastRefreshTime(video.getPublishedAt());
+		assert (BATCH_SIZE <= 50);
+		Collection<Callable<VideoListResponse>> tasks = new ArrayList<>();
+		for (int i = 0; i * BATCH_SIZE < ids.size(); i++) {
+			int start = BATCH_SIZE * i;
+			int end = start + BATCH_SIZE < ids.size() ? start + BATCH_SIZE
+					: ids.size();
+			List<String> subList = ids.subList(start, end);
+			tasks.add(new GetVideosTask(subList));
+		}
+		try {
+			List<Future<VideoListResponse>> results = Application.getExecutor()
+					.invokeAll(tasks);
+			for (Future<VideoListResponse> result : results) {
+				VideoListResponse videoListResponse = result.get();
+				for (Video v : videoListResponse.getItems()) {
+					YTVideo video = new YTVideo(v);
+					Channel channel = getChannel(video.getChannelId());
+					video.setChannelName(channel.getTitle());
+					videos.add(video);
+					if (video.getPublishedAt().getValue() > channel
+							.getLastRefreshTime().getValue()) {
+						channel.setLastRefreshTime(video.getPublishedAt());
+					}
 				}
 			}
+		} catch (InterruptedException | ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-
-		Collections.sort(videos);
 		return videos;
 	}
 
-	private List<String> getIds(Collection<Channel> channels)
+	private List<String> getVideoIds(Collection<Channel> channels)
 			throws IOException {
 		List<String> ids = new ArrayList<>();
 		Collection<Callable<List<String>>> tasks = new ArrayList<>();
 		for (Channel channel : channels) {
-			tasks.add(new GetVideosTask(channel));
+			tasks.add(new GetVideoIdsTask(channel));
 		}
-		
+
 		try {
-			List<Future<List<String>>> results = Application.getExecutor().invokeAll(tasks);
+			List<Future<List<String>>> results = Application.getExecutor()
+					.invokeAll(tasks);
 			for (Future<List<String>> result : results) {
 				for (String id : result.get()) {
 					if (!this.videos.containsKey(id)) {
@@ -193,52 +191,36 @@ public class User {
 					}
 				}
 			}
-		} catch (InterruptedException e) {
+		} catch (InterruptedException| ExecutionException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (ExecutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		} 
 		return ids;
 	}
-	
 
-	private List<Channel> getSubscriptionsFromYouTube()
-			throws IOException {
+	private List<Channel> getSubscriptionsFromYouTube() throws IOException {
 
 		List<Channel> channelList = new ArrayList<>();
-
 		String nextPageToken = "";
 
 		do {
 			SubscriptionListResponse subscriptionListResponse = null;
 			try {
 				subscriptionListResponse = youtube.subscriptions()
-						.list("snippet")
-						.setMine(true)
-						.setMaxResults((long) 50)
-						.setPageToken(nextPageToken)
-						.execute();
+						.list("snippet").setMine(true).setMaxResults((long) 50)
+						.setPageToken(nextPageToken).execute();
 			} catch (TokenResponseException | GoogleJsonResponseException e) {
 				// TODO Auto-generated catch block
 				System.out.println(e);
-				// Auth.deleteUserFromCredentialDataStore(request.getSession()
-				// .getId());
-				// response.sendRedirect("/Test/home");
-				// return;
 			}
 			nextPageToken = subscriptionListResponse.getNextPageToken();
 			for (Subscription sub : subscriptionListResponse.getItems()) {
 				channelList.add(new Channel(sub));
 			}
-		} while (nextPageToken!=null && nextPageToken.length() > 0); 
+		} while (nextPageToken != null && nextPageToken.length() > 0);
 
 		return channelList;
 	}
-
-	
 
 	public YTVideo getVideo(String videoId) {
 		return videos.get(videoId);
@@ -255,8 +237,9 @@ public class User {
 	public Collection<Channel> getFilters() {
 		Collection<Channel> list = new ArrayList<>();
 		for (String channelId : filters.keySet()) {
-			Channel channel = subscriptions.get(channelId);//TODO channel can be null
-			if (channel.isActive()) {
+			Channel channel = subscriptions.get(channelId);// TODO channel can
+															// be null
+			if (channel!=null && channel.isActive()) {
 				channel.setFilters(filters.get(channelId));
 				list.add(channel);
 			}
@@ -278,18 +261,35 @@ public class User {
 			filters.put(channelId, list);
 		}
 	}
-	
-	private final class GetVideosTask implements Callable<List<String>>{
+
+	private final class GetVideoIdsTask implements Callable<List<String>> {
 		private final Channel channel;
-		GetVideosTask(Channel channel){
+
+		GetVideoIdsTask(Channel channel) {
 			this.channel = channel;
 		}
+
 		@Override
 		public List<String> call() throws Exception {
-			
+
 			return channel.getVideos(youtube);
 		}
-		
+	}
+
+	private final class GetVideosTask implements Callable<VideoListResponse> {
+		private final Collection<String> ids;
+
+		GetVideosTask(Collection<String> ids) {
+			this.ids = ids;
+		}
+
+		@Override
+		public VideoListResponse call() throws Exception {
+			String idsString = String.join(",", ids);
+			return youtube.videos().list("snippet,contentDetails")
+					.setId(idsString).execute();
+		}
+
 	}
 
 }
